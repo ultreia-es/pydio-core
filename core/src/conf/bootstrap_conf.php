6ed6564bb060c91ef76f5a08dffd197bd763fff5
Jesús Pérez (Ultreia) <jperez@ultreia.es>
2013-05-27 10:40:48 +0000

    Set default locale to Spanish.

--- core/src/conf/bootstrap_conf.php
+++ core/src/conf/bootstrap_conf.php
@@ -29,7 +29,7 @@
  * Windows users may define an empty string
  * define("AJXP_LOCALE", "");
  */
-//define("AJXP_LOCALE", "en_EN.UTF-8");
+define("AJXP_LOCALE", "es_ES.UTF-8");
 //define("AJXP_LOCALE", "");
 
 
