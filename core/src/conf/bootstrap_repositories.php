3a73f1d8d4363057d3fb0278161dc3827d1af16c
Jesús Pérez (Ultreia) <jperez@ultreia.es>
2013-05-29 16:44:15 +0200

    Configure repositories.

--- core/src/conf/bootstrap_repositories.php
+++ core/src/conf/bootstrap_repositories.php
@@ -1,22 +1,22 @@
 <?php
 /*
- * Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
- * This file is part of Pydio.
+ * Copyright 2007-2011 Charles du Jeu <contact (at) cdujeu.me>
+ * This file is part of AjaXplorer.
  *
- * Pydio is free software: you can redistribute it and/or modify
+ * AjaXplorer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Pydio is distributed in the hope that it will be useful,
+ * AjaXplorer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ * along with AjaXplorer.  If not, see <http://www.gnu.org/licenses/>.
  *
- * The latest code can be found at <http://pyd.io/>.
+ * The latest code can be found at <http://www.ajaxplorer.info/>.
  *
  * Description : configuration file
  * BASIC REPOSITORY CONFIGURATION.
@@ -27,126 +27,68 @@
 defined('AJXP_EXEC') or die( 'Access not allowed');
 
 $REPOSITORIES[0] = array(
-    "DISPLAY"		=>	"Default Files",
+	"DISPLAY"		=>	"Default Files",
     "DISPLAY_ID"    =>  430,
     "DESCRIPTION_ID"=>  475,
-    "AJXP_SLUG"		=>  "default",
-    "DRIVER"		=>	"fs",
-    "DRIVER_OPTIONS"=> array(
-        "PATH"			=>	"AJXP_DATA_PATH/files",
-        "CREATE"		=>	true,
-        "RECYCLE_BIN" 	=> 	'recycle_bin',
-        "CHMOD_VALUE"   =>  '0600',
-        "DEFAULT_RIGHTS"=>  "",
-        "PAGINATION_THRESHOLD" => 500,
-        "PAGINATION_NUMBER" => 200,
-        "META_SOURCES"		=> array(
-            "metastore.serial"=> array(
-                "METADATA_FILE"	=> ".ajxp_meta",
-                "METADATA_FILE_LOCATION" => "infolders"
-            ),
-            "meta.user"     => array(
-                "meta_fields"		=> "tags",
-                "meta_labels"		=> "Tags",
-                "meta_visibility"   => "hidden"
-            ),
-            "meta.filehasher"   => array(),
-            "meta.watch"        => array(),
-            "meta.exif"   => array(
-                "meta_fields" => "COMPUTED_GPS.GPS_Latitude,COMPUTED_GPS.GPS_Longitude",
-                "meta_labels" => "Latitude,Longitude"
-            ),
-            "index.lucene" => array(
-                "index_meta_fields" => "tags"
-            ),
-        )
-    ),
-
-);
-
-$REPOSITORIES[1] = array(
-    "DISPLAY"		=>	"My Files",
-    "DISPLAY_ID"    =>  432,
-    "DESCRIPTION_ID"=>  476,
-    "AJXP_SLUG"		=>  "my-files",
-    "DRIVER"		=>	"fs",
-    "DRIVER_OPTIONS"=> array(
-        "PATH"			=>	"AJXP_DATA_PATH/personal/AJXP_USER",
-        "CREATE"		=>	true,
-        "RECYCLE_BIN" 	=> 	'recycle_bin',
-        "CHMOD_VALUE"   =>  '0600',
-        "DEFAULT_RIGHTS"=>  "rw",
-        "PAGINATION_THRESHOLD" => 500,
-        "PAGINATION_NUMBER" => 200,
-        "META_SOURCES"		=> array(
-            "metastore.serial"=> array(
-                "METADATA_FILE"	=> ".ajxp_meta",
-                "METADATA_FILE_LOCATION" => "infolders"
-            ),
-            "meta.user"     => array(
-                "meta_fields"		=> "tags",
-                "meta_labels"		=> "Tags",
-                "meta_visibility"   => "hidden"
-            ),
-            "meta.filehasher"   => array(),
-            "meta.watch"        => array(),
-            "meta.exif"   => array(
-                "meta_fields" => "COMPUTED_GPS.GPS_Latitude,COMPUTED_GPS.GPS_Longitude",
-                "meta_labels" => "Latitude,Longitude"
-            ),
-            "index.lucene" => array(
-                "index_meta_fields" => "tags",
-                "repository_specific_keywords" => "AJXP_USER",
-            )
-        )
-    ),
-
+	"AJXP_SLUG"		=>  "default",
+	"DRIVER"		=>	"ftp",
+	"DRIVER_OPTIONS"=> array(
+		"FTP_HOST" => "localhost",
+		"FTP_PORT" => 21,
+		"PATH" => "/",
+		"FTP_DIRECT" => true,
+		"USE_SESSION_CREDENTIALS" => true,
+		"RECYCLE_BIN" => "recycle_bin",
+		"CHMOD_VALUE" => "0600",
+		"DEFAULT_RIGHTS"=>  "rw",
+		"PAGINATION_THRESHOLD" => 500,
+		"PAGINATION_NUMBER" => 200,
++		"TMP_UPLOAD" => "/tmp"
+		)
 );
 
 // DO NOT REMOVE THIS!
-// USER DASHBOARD
-$REPOSITORIES["ajxp_user"] = array(
-    "DISPLAY"		    =>	"My Dashboard",
-    "DISPLAY_ID"		=>	"user_dash.title",
-    "DESCRIPTION_ID"	=>	"user_dash.desc",
-    "DRIVER"		    =>	"ajxp_user",
-    "DRIVER_OPTIONS"    => array(
-        "DEFAULT_RIGHTS" => "rw"
-    )
+// SHARE ELEMENTS
+$REPOSITORIES["ajxp_shared"] = array(
+	"DISPLAY"		=>	"Shared Elements",
+	"DISPLAY_ID"		=>	"467",
+	"DRIVER"		=>	"ajxp_shared",
+	"DRIVER_OPTIONS"=> array(
+		"DEFAULT_RIGHTS" => "rw"
+	)
 );
 
 // ADMIN REPOSITORY
 $REPOSITORIES["ajxp_conf"] = array(
-    "DISPLAY"		=>	"Settings",
-    "DISPLAY_ID"		=>	"165",
-    "DESCRIPTION_ID"	=>	"506",
-    "DRIVER"		=>	"ajxp_conf",
-    "DRIVER_OPTIONS"=> array()
+	"DISPLAY"		=>	"Settings",
+	"DISPLAY_ID"		=>	"165",
+	"DRIVER"		=>	"ajxp_conf",
+	"DRIVER_OPTIONS"=> array()
 );
 
 $REPOSITORIES["fs_template"] = array(
-    "DISPLAY"		=>	"Sample Template",
+	"DISPLAY"		=>	"Sample Template",
     "DISPLAY_ID"    =>  431,
-    "IS_TEMPLATE"	=>  true,
-    "DRIVER"		=>	"fs",
-    "DRIVER_OPTIONS"=> array(
-        "CREATE"		=>	true,
-        "RECYCLE_BIN" 	=> 	'recycle_bin',
-        "CHMOD_VALUE"   =>  '0600',
-        "PAGINATION_THRESHOLD" => 500,
-        "PAGINATION_NUMBER" => 200,
+	"IS_TEMPLATE"	=>  true,
+	"DRIVER"		=>	"fs",
+	"DRIVER_OPTIONS"=> array(
+		"CREATE"		=>	true,
+		"RECYCLE_BIN" 	=> 	'recycle_bin',
+		"CHMOD_VALUE"   =>  '0600',
+		"PAGINATION_THRESHOLD" => 500,
+		"PAGINATION_NUMBER" => 200,
         "PURGE_AFTER"       => 0,
         "CHARSET"           => "",
-        "META_SOURCES"		=> array(
-            "metastore.serial"=> array(
-                "METADATA_FILE"	=> ".ajxp_meta",
+		"META_SOURCES"		=> array(
+			"metastore.serial"=> array(
+				"METADATA_FILE"	=> ".ajxp_meta",
                 "METADATA_FILE_LOCATION" => "infolders"
             ),
             "meta.user"     => array(
-                "meta_fields"		=> "tags",
-                "meta_labels"		=> "Tags",
+				"meta_fields"		=> "tags",
+				"meta_labels"		=> "Tags",
                 "meta_visibility"   => "hidden"
-            ),
+			),
             "meta.filehasher"   => array(),
             "meta.watch"        => array(),
             "meta.exif"   => array(
@@ -156,7 +98,7 @@ $REPOSITORIES["fs_template"] = array(
             "index.lucene" => array(
                 "index_meta_fields" => "tags"
             )
-        )
-    ),
+		)
+	),
 
 );
